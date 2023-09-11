7024497e7517277fd38de0ab8e65f94273c2c1ca
“rakibul8226” <rakibul8226@gmail.com>
2023-09-11 23:50:52 +0600

    complete

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 
  let totalPrice = 0;
+ const pButton = document.getElementById('purchase-btn')
 function calculateTotalPrice(target){
     // const totalPrice = getElementValue('item')
     const itemName = target.childNodes[5].innerText;
@@ -8,27 +9,33 @@ function calculateTotalPrice(target){
     const cartItemContainer = document.getElementById("cart-item");
     cartItemContainer.appendChild(li);
 
-
-
     const itemPrice = target.childNodes[7].childNodes[1].childNodes[0].innerText;
     totalPrice = parseInt(totalPrice) + parseInt(itemPrice);
    
     const currentTotalPrice = document.getElementById('total-price');
     currentTotalPrice.innerText = totalPrice;
  
+        //enable purchase  button  
+        const totalProducts = totalPrice;
+        const button = document.getElementById('activate-button');
+
+        if (totalProducts >= 1) {
+            pButton.removeAttribute('disabled');
+        } else {
+            pButton.setAttribute('disabled', 'true');
+        }
+        
+        //enable coupon button
+        if (totalProducts >= 200) {
+            button.removeAttribute('disabled');
+        } else {
+            button.setAttribute('disabled', 'true');
+        }
 
 
-    //enable button
-    const totalProducts = totalPrice;
-    const button = document.getElementById('activate-button');
-    if (totalProducts >= 200) {
-        button.removeAttribute('disabled');
-    } else {
-        button.setAttribute('disabled', 'true');
-    }
-
-   
   }
+
+
 
     //applying discount on code
 
@@ -40,7 +47,7 @@ function calculateTotalPrice(target){
         if (couponCode === 'SELL200') {
             
             const discountedPrice = totalPrice * 0.8; // 20% discount
-            alert(`Coupon code "SELL200" applied. Discounted price: $${discountedPrice.toFixed(2)}`);
+            alert('Coupon code "SELL200" applied');
 
             const totalFinalAmount = document.getElementById('total-final');
             totalFinalAmount.innerText = discountedPrice;
@@ -54,8 +61,3 @@ function calculateTotalPrice(target){
             alert('Invalid coupon code. Please try again.');
         }
     }
-    
- 
-
-
-
