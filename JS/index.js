47775eb4a22e6cdd6120b8b16aff625a9dc51d8e
“rakibul8226” <rakibul8226@gmail.com>
2023-09-11 19:37:51 +0600

    function

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,61 @@
-function check(){
-    console.log("connected");
-}
+
+ let totalPrice = 0;
+function calculateTotalPrice(target){
+    // const totalPrice = getElementValue('item')
+    const itemName = target.childNodes[5].innerText;
+    const li = document.createElement('li');
+    li.innerText = itemName;    
+    const cartItemContainer = document.getElementById("cart-item");
+    cartItemContainer.appendChild(li);
+
+
+
+    const itemPrice = target.childNodes[7].childNodes[1].childNodes[0].innerText;
+    totalPrice = parseInt(totalPrice) + parseInt(itemPrice);
+   
+    const currentTotalPrice = document.getElementById('total-price');
+    currentTotalPrice.innerText = totalPrice;
+ 
+
+
+    //enable button
+    const totalProducts = totalPrice;
+    const button = document.getElementById('activate-button');
+    if (totalProducts >= 200) {
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', 'true');
+    }
+
+   
+  }
+
+    //applying discount on code
+
+    function applyDiscount() {
+
+        
+        const couponCode = document.getElementById('coupon-code').value;
+        
+        if (couponCode === 'SELL200') {
+            
+            const discountedPrice = totalPrice * 0.8; // 20% discount
+            alert(`Coupon code "SELL200" applied. Discounted price: $${discountedPrice.toFixed(2)}`);
+
+            const totalFinalAmount = document.getElementById('total-final');
+            totalFinalAmount.innerText = discountedPrice;
+
+            const totalDiscount = totalPrice - discountedPrice;
+            const discountElement = document.getElementById('discounted-price');
+            discountElement.innerText = totalDiscount;
+
+            
+        } else {
+            alert('Invalid coupon code. Please try again.');
+        }
+    }
+    
+ 
+
+
+
